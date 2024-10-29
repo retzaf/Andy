1bd41af1cdce84bc28d7b53aa41fc88f7e8f0816
retzaf <varasandy567@gmail.com>
2024-10-29 16:21:30 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -6,7 +6,7 @@ st.sidebar.title("insertar texto")
 st.sidebar.header("insertar texto")
 st.sidebar.write("insertar texto")
 
-st.sidebar.image("logo-clinica-davila.jpg")
+st.sidebar.image("DEFINICIONES-PNG.jpg")
 if st.sidebar.button("insertar texto"):
     st.sidebar.write("insertar texto")
  

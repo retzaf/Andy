e5a849ac43f73f22f7731efd5a3d649938dc8967
retzaf <varasandy567@gmail.com>
2024-10-29 15:55:02 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.sidebar.title("Mi primera barra lateral de streamlit")
-st.sidebar.header("Hola barra lateral")
-st.sidebar.write("esto es una barra lateral")
+st.sidebar.title("aleatorio")
+st.sidebar.header("aleatorio")
+st.sidebar.write("aleatorio")
 
 st.sidebar.image("images.jpg")
-if st.sidebar.button("Presiona aquí"):
-    st.sidebar.write("Has presionado el botón")
+if st.sidebar.button("aleatorio"):
+    st.sidebar.write("aleatorio")
  
-user_input = st.sidebar.text_input("Escribe tu nombre")
-st.sidebar.write("Hola", user_input)
+user_input = st.sidebar.text_input("aleatorio")
+st.sidebar.write("aleatorio", user_input)

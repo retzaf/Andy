206b68b79135d865715613b06768a35c04a597c6
retzaf <varasandy567@gmail.com>
2024-10-29 16:16:39 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.sidebar.title("aleatorio")
-st.sidebar.header("aleatorio")
-st.sidebar.write("aleatorio")
+st.sidebar.title("clinica davila won")
+st.sidebar.header("va pegao el loco")
+st.sidebar.write("va pegao")
 
 st.sidebar.image("logo-clinica-davila.jpg")
-if st.sidebar.button("aleatorio"):
-    st.sidebar.write("aleatorio")
+if st.sidebar.button("va pegao loco won"):
+    st.sidebar.write("fue el loco")
  
-user_input = st.sidebar.text_input("aleatorio")
-st.sidebar.write("aleatorio", user_input)
+user_input = st.sidebar.text_input("ya fue")
+st.sidebar.write("iba pegao el", user_input)
